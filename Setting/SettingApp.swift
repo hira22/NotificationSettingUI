e0b8321ebb8be0f64d70a707211f4e852d9b7418
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-12-09 19:15:50 +0900

    Initial Commit

---
+++ Setting/SettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingApp.swift
+//  Setting
+//
+//  Created by hiraoka on 2020/12/09.
+//
+
+import SwiftUI
+
+@main
+struct SettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
